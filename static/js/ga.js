b7da3c70fff7ffbd134c1f1eb95a5d71e0d6f15d
Rui Ramos <rui.ms.ramos@gmail.com>
2025-03-10 23:35:20 +0000

    GA consent nothing

--- static/js/ga.js
+++ static/js/ga.js
@@ -4,6 +4,13 @@ _gaq.push(['_gat._forceSSL']);
 _gaq.push(['_gat._anonymizeIp']);
 _gaq.push(['_trackPageview']);
 
+gtag('consent', 'default', {
+  'ad_storage': 'denied',
+  'ad_user_data': 'denied',
+  'ad_personalization': 'denied',
+  'analytics_storage': 'denied'
+});
+
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
